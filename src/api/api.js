7b32c4e798d9280d07a73538c4adc9893b4eeed1
Abdelhalim Misho <abdelhalimyahiaoui94@gmail.com>
2025-05-14 16:29:45 +0100

    update7

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
-const production = 'https://soug-elfalah-backend-5.onrender.com'
+const production = 'https://soug-elfalah-backend-6.onrender.com'
 
 let api_url =''
 let mode ='pro'
