eb1c4e85921e4ad2989f748415b59f0bcd452384
Abdelhalim Misho <abdelhalimyahiaoui94@gmail.com>
2025-05-14 14:33:29 +0100

    update1

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
+const local = 'http://localhost:5000'
+const production = 'https://soug-elfalah-backend-2.onrender.com'
 
+let api_url =''
+let mode ='pro'
+if (mode === 'pro'){
+    api_url = production
+}else{
+    api_url = local
+}
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api',
-    withCredentials: true // <--- This is ESSENTIAL
+    baseURL : `${api_url}/api`,
+    withCredentials: true
 })
 
 export default api
